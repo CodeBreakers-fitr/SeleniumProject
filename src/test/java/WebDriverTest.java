6f456944505c95b45fd8b5deb958de9ef6c01238
Volha <telolya@gmail.com>
2024-11-10 00:36:11 +0300

    Initial commit

---
+++ src/test/java/WebDriverTest.java
@@ -0,0 +1,8 @@
+import org.openqa.selenium.JavascriptException;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
